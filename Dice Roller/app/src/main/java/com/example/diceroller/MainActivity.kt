d7fe96678ca11e1a27265c9b1d256420962403da
David N. de la Rosa L. <89267615+DaviddelaRosa15@users.noreply.github.com>
2025-09-07 18:41:30 -0400

    Exercise: Finding Views Efficiently
    Moved diceImage initialization to onCreate and made it a class property to avoid repeated calls to findViewById in rollDice(). This improves code efficiency and readability.

--- Dice Roller/app/src/main/java/com/example/diceroller/MainActivity.kt
+++ Dice Roller/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -7,6 +7,8 @@ import androidx.activity.ComponentActivity
 import java.util.Random
 
 class MainActivity : ComponentActivity() {
+    lateinit var diceImage: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -14,6 +16,8 @@ class MainActivity : ComponentActivity() {
         rollButton.setOnClickListener {
             rollDice()
         }
+
+        diceImage = findViewById(R.id.dice_image)
         /*enableEdgeToEdge()
         setContent {
             DiceRollerTheme {
@@ -28,9 +32,7 @@ class MainActivity : ComponentActivity() {
     }
 
     private fun rollDice() {
-        val diceImage: ImageView = findViewById(R.id.dice_image)
         val randomInt = Random().nextInt(6) + 1
-
         val drawableResource = when (randomInt) {
             1 -> R.drawable.dice_1
             2 -> R.drawable.dice_2
