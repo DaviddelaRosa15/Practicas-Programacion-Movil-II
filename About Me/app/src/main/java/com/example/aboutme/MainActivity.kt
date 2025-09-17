d6e6b6b82cb75eac475659b10dccc4eeac07afb9
David N. de la Rosa L. <89267615+DaviddelaRosa15@users.noreply.github.com>
2025-09-16 22:18:36 -0400

    Exercise: Add a TextView, ImageView, and Styling
    Replaced Jetpack Compose setup with XML layout in MainActivity. Added a TextView for the user's name and an ImageView for a star icon in activity_main.xml. Introduced custom font (Roboto), new dimens, strings, and a style for the name text.

--- About Me/app/src/main/java/com/example/aboutme/MainActivity.kt
+++ About Me/app/src/main/java/com/example/aboutme/MainActivity.kt
@@ -16,17 +16,7 @@ import com.example.aboutme.ui.theme.AboutMeTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            AboutMeTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
-        }
+        setContentView(R.layout.activity_main)
     }
 }
 
