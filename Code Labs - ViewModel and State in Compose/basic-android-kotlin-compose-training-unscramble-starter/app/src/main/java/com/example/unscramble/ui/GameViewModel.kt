13d8c1b09d08678cb54527e069545cfd3c62f058
David N. de la Rosa L. <89267615+DaviddelaRosa15@users.noreply.github.com>
2025-09-18 21:47:17 -0400

    Practica 3 Unscramble Compose app project
    Initial commit of the basic Android Kotlin Compose Unscramble app starter project. Includes app source code, resources, Gradle configuration, and theme files for the ViewModel and State in Compose code lab.

---
+++ Code Labs - ViewModel and State in Compose/basic-android-kotlin-compose-training-unscramble-starter/app/src/main/java/com/example/unscramble/ui/GameViewModel.kt
@@ -0,0 +1,51 @@
+package com.example.unscramble.ui
+
+import androidx.lifecycle.ViewModel
+import com.example.unscramble.data.allWords
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+
+class GameViewModel : ViewModel() {
+
+    // Game UI state
+    private val _uiState = MutableStateFlow(GameUiState())
+    val uiState: StateFlow<GameUiState> = _uiState.asStateFlow()
+    // Current scrambled word
+    private lateinit var currentWord: String
+    // Set of words used in the game
+    private var usedWords: MutableSet<String> = mutableSetOf()
+
+    // Initialize the game
+    init {
+        resetGame()
+    }
+
+    // Pick a random word and shuffle it
+    private fun pickRandomWordAndShuffle(): String {
+        // Continue picking up a new random word until you get one that hasn't been used before
+        currentWord = allWords.random()
+        if (usedWords.contains(currentWord)) {
+            return pickRandomWordAndShuffle()
+        } else {
+            usedWords.add(currentWord)
+            return shuffleCurrentWord(currentWord)
+        }
+    }
+
+    // Shuffle the letters of the current word
+    private fun shuffleCurrentWord(word: String): String {
+        val tempWord = word.toCharArray()
+        // Scramble the word
+        tempWord.shuffle()
+        while (String(tempWord).equals(word)) {
+            tempWord.shuffle()
+        }
+        return String(tempWord)
+    }
+
+    fun resetGame() {
+        usedWords.clear()
+        _uiState.value = GameUiState(currentScrambledWord = pickRandomWordAndShuffle())
+    }
+}
