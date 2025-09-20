61c211675ec8c2f50f52265bb2f6b3148bdc1d5e
David N. de la Rosa L. <89267615+DaviddelaRosa15@users.noreply.github.com>
2025-09-20 11:39:29 -0400

    Exercise: Add Aligned Boxes with Click Handlers
    Enabled view binding in build.gradle and refactored MainActivity to use it. Added four new TextViews (boxes) to the layout, updated styles for white background, and provided string resources for each box. Implemented click listeners to change box colors dynamically.

--- ColorMyViews/app/src/main/java/com/example/android/aboutme/MainActivity.kt
+++ ColorMyViews/app/src/main/java/com/example/android/aboutme/MainActivity.kt
@@ -16,9 +16,11 @@
 
 package com.example.android.colormyviews
 
+import android.graphics.Color
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-
+import com.example.android.colormyviews.databinding.ActivityMainBinding
 
 /**
  * Main Activity of the colormyviews app. This app demonstrates:
@@ -32,8 +34,39 @@ import androidx.appcompat.app.AppCompatActivity
  */
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setListeners()
+    }
+
+    private  fun setListeners() {
+        val clickableViews: List<View> =
+            listOf(binding.boxOneText, binding.boxTwoText, binding.boxThreeText,
+                binding.boxFourText, binding.boxFiveText, binding.constraintLayout)
+
+        for (item in clickableViews) {
+            item.setOnClickListener { makeColored(it) }
+        }
+    }
+
+    fun makeColored(view: View) {
+        when (view.id) {
+
+            // Boxes using Color class colors for background
+            R.id.box_one_text -> view.setBackgroundColor(Color.DKGRAY)
+            R.id.box_two_text -> view.setBackgroundColor(Color.GRAY)
+
+            // Boxes using Android color resources for background
+            R.id.box_three_text -> view.setBackgroundResource(android.R.color.holo_green_light)
+            R.id.box_four_text -> view.setBackgroundResource(android.R.color.holo_green_dark)
+            R.id.box_five_text -> view.setBackgroundResource(android.R.color.holo_green_light)
+
+            else -> view.setBackgroundColor(Color.LTGRAY)
+        }
     }
 }
